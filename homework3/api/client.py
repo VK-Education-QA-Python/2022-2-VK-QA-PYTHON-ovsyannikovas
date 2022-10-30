927a3a85047bc0da5e1379522ff3e82b0bc36775
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-10-30 10:07:47 +0300

    test_api_login works

--- homework3/api/client.py
+++ homework3/api/client.py
@@ -19,13 +19,10 @@ class ApiClient:
         }
         headers = {
             'Referer': 'https://account.my.com/',
-            'Cookie': ''
         }
         # getting mc and ssdc cookies
         self.session.post(url="https://auth-ac.my.com/auth?lang=ru&nosavelogin=0", allow_redirects=False,
                           headers=headers, data=data)
-        # cookies_dict = dict(self.session.cookies)
-        # headers['Cookie'] += f'mc={cookies_dict["mc"]}; ssdc={cookies_dict["ssdc"]}; '
 
         # getting token for next url
         token = self.session.get(
@@ -35,13 +32,9 @@ class ApiClient:
 
         # getting sdcs cookie
         self.session.get(url=f"https://account.my.com/sdc?token={token}")
-        # cookies_dict = dict(self.session.cookies)
-        # headers['Cookie'] += f'sdcs={cookies_dict["sdcs"]}; '
 
         # getting sdc and csrftoken cookies
         self.session.get(url=f"https://target-sandbox.my.com/csrf/")
-        # cookies_dict = dict(self.session.cookies)
-        # headers['Cookie'] += f'sdc={cookies_dict["sdc"]}; csrftoken={cookies_dict["csrftoken"]}; '
 
         login_request = self.session.post(url='https://target-sandbox.my.com/', headers=headers,
                                           data=data)
