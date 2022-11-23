5254a822c3fdae41f40882e60c2d5c1ecf429279
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-11-23 20:07:45 +0300

    Убрана параметризация, тесты разнесены на отдельные

--- homework6/test_sql/test_sql.py
+++ homework6/test_sql/test_sql.py
@@ -5,11 +5,17 @@ from base import MyTest
 
 
 class TestMysql(MyTest):
+    def test_table_len_task1(self):
+        assert self.get_model_len(Task1) == len(self.builder.data[1])
 
-    @pytest.mark.parametrize('model, index', [(Task1, 1),
-                                              (Task2, 2),
-                                              (Task3, 3),
-                                              (Task4, 4),
-                                              (Task5, 5)])
-    def test_table_len(self, model, index):
-        assert self.get_model_len(model) == len(self.builder.data[index])
+    def test_table_len_task2(self):
+        assert self.get_model_len(Task2) == len(self.builder.data[2])
+
+    def test_table_len_task3(self):
+        assert self.get_model_len(Task3) == len(self.builder.data[3])
+
+    def test_table_len_task4(self):
+        assert self.get_model_len(Task4) == len(self.builder.data[4])
+
+    def test_table_len_task5(self):
+        assert self.get_model_len(Task5) == len(self.builder.data[5])
